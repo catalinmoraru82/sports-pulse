5d5fc2dd3afd1046f784880555e69c81f1c49349
catalinmoraru82 <catalinmoraru82@gmail.com>
2026-08-21 00:05:26 +0300

    sync android app with admin

---
+++ app/build/generated/source/buildConfig/debug/com/sportspulse/app/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.sportspulse.app;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.sportspulse.app";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String ANDROID_API_KEY = "2ca73d56a0187a3yh4dd2a7uyc6f76241eecbaiuj7b1c89006b83";
+  // Field from default config.
+  public static final String API_BASE_URL = "https://sports-admin-azure.vercel.app/";
+}
